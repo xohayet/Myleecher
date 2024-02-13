dba04a10bf8c88fb40ed608e426511978fef4ea4
xohayet <157043654+xohayet@users.noreply.github.com>
2024-02-13 22:16:25 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 26435700
-api_hash = "527cf5174e120a9093611bc69d7b7709"
-bot_token = "6180767554:AAGKw-Eo6L0DXlL4BsrDmKItUewgdKGAtGo"
+api_id = 24906331
+api_hash = "866e8e4637fb269388b50202fb0f169c"
+bot_token = "6921841186:AAEMTH5ZR6DKRUYmwzptN5yG1_HafRsro38"
 
